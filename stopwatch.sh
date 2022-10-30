c87ee9ebac3f6fdfeef4a294892b52fecc35ee1d
justin-brewer <jarvis@cpe-72-190-57-241.tx.res.rr.com>
2022-10-30 15:14:58 -0500

    printf formatting

--- stopwatch.sh
+++ stopwatch.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+
 stop_watch () 
 {
     time=0
     while :
     do
-        printf “%s\r” “$(convertsecs $time)”
+        printf "%s\r" "$(convertsecs $time)"
         sleep 1
         time=$((time+1))
     done
 }
+
 convertsecs() {
     ((h=${1}/3600))
     ((m=(${1}%3600)/60))
     ((s=${1}%60))
-    printf “%02d:%02d:%02d\n” $h $m $s
+    printf "%02d:%02d:%02d\n" $h $m $s
 }
+
 stop_watch
